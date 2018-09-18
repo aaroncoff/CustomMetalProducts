3d263d00480f3c45d4052242d22e4b6f41709921
softcomet <realseniordev210@gmail.com>
2018-09-18 10:39:47 +0200

    minor issue fix

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,11 +6,11 @@ export default class About extends Component{
         return(
         <div className="aboutParent">
             <div className="aboutContainer">
-                <div className='person1'><img src={require('../Media/Images/Staff/small Jonas Wadell PRESIDENT & CEO.jpg')} alt={'Jonas'} className={'Jonas'}></img><p>Jonas Waddell - President & CEO</p></div>
-                <div className='person2'><img src={require('../Media/Images/Staff/Jeanine Waddell VICE PRESIDENT.jpg')} alt={'Jeanine'} className={'Jeanine'}></img><p>Jeanine Waddell - Vice President</p></div>
-                <div className='person5'><img src={require('../Media/Images/Staff/small Rhonda Crow HR MANAGER.jpg')} alt={'Rhonda'} className={'Rhonda'}></img><p>Rhonda Crow - HR Manager</p></div>
-                <div className='person3'><img src={require('../Media/Images/Staff/Blake Brumbelow PROJECT MANAGER & SALES (1).jpg')} alt={'Blake'} className={'Blake'}></img><p>Blake Brumbelow - Sales & Project Manager</p></div>
-                <div className='person4'><img src={require('../Media/Images/Staff/small Lee Carroll OPERATIONS MANAGER.jpg')} alt={'Lee'} className={'Lee'}></img><p>Lee Carroll - Operations Manager</p></div>
+                <div className='person1'><img src={require('../Media/Images/Staff/1.jpg')} alt={'Jonas'} className={'Jonas'}></img><p>Jonas Waddell - President & CEO</p></div>
+                <div className='person2'><img src={require('../Media/Images/Staff/2.jpg')} alt={'Jeanine'} className={'Jeanine'}></img><p>Jeanine Waddell - Vice President</p></div>
+                <div className='person5'><img src={require('../Media/Images/Staff/3.jpg')} alt={'Rhonda'} className={'Rhonda'}></img><p>Rhonda Crow - HR Manager</p></div>
+                <div className='person3'><img src={require('../Media/Images/Staff/4.jpg')} alt={'Blake'} className={'Blake'}></img><p>Blake Brumbelow - Sales & Project Manager</p></div>
+                <div className='person4'><img src={require('../Media/Images/Staff/5.jpg')} alt={'Lee'} className={'Lee'}></img><p>Lee Carroll - Operations Manager</p></div>
 
             </div>
         </div>
