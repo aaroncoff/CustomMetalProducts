c9c3a947ef02ecc6050c4096c59f245a9abfe104
kenyx89 <kenyx.my@gmail.com>
2018-10-18 14:50:00 +0800

    Fixing profile names

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,10 +14,10 @@ export default class About extends Component{
 
             </p>
             <div className="aboutContainer">
-                <div className='person1'><img src={require('../Media/Images/Staff/small Jonas Wadell PRESIDENT & CEO.jpg')} alt={'Jonas'} className={'Jonas'}></img><p>Jonas Waddell - President & CEO</p></div>
+                <div className='person1'><img src={require('../Media/Images/Staff/1.jpg')} alt={'Jonas'} className={'Jonas'}></img><p>Jonas Waddell - President & CEO</p></div>
                 {/* <div className='person2'><img src={require('../Media/Images/Staff/Jeanine Waddell VICE PRESIDENT.JPG')} alt={'Jeanine'} className={'Jeanine'}></img><p>Jeanine Waddell - Vice President</p></div> */}
-                <div className='person5'><img src={require('../Media/Images/Staff/small Rhonda Crow HR MANAGER.jpg')} alt={'Rhonda'} className={'Rhonda'}></img><p>Rhonda Crow - HR Manager</p></div>
-                <div className='person3'><img src={require('../Media/Images/Staff/Blake Brumbelow PROJECT MANAGER & SALES (1).jpg')} alt={'Blake'} className={'Blake'}></img><p>Blake Brumbelow - Sales & Project Manager</p></div>
+                <div className='person5'><img src={require('../Media/Images/Staff/3.jpg')} alt={'Rhonda'} className={'Rhonda'}></img><p>Rhonda Crow - HR Manager</p></div>
+                <div className='person3'><img src={require('../Media/Images/Staff/4.jpg')} alt={'Blake'} className={'Blake'}></img><p>Blake Brumbelow - Sales & Project Manager</p></div>
 
             </div>
         </div>
